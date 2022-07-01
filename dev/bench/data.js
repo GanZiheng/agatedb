34787e441ffd333e2f81aa1be076ed846d58907c
github-action-benchmark <github@users.noreply.github.com>
2022-07-01 09:11:48 +0000

    add Benchmark (cargo) benchmark result for 5cac49a2976d5bab9c40fb647dc5239b46758e76

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1656664938093,
+  "lastUpdate": 1656666708554,
   "repoUrl": "https://github.com/GanZiheng/agatedb",
   "entries": {
     "Benchmark": [
@@ -59,6 +59,66 @@ window.BENCHMARK_DATA = {
             "name": "table random read",
             "value": 1802,
             "range": "± 33",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ganziheng98@gmail.com",
+            "name": "GanZiheng",
+            "username": "GanZiheng"
+          },
+          "committer": {
+            "email": "ganziheng98@gmail.com",
+            "name": "GanZiheng",
+            "username": "GanZiheng"
+          },
+          "distinct": true,
+          "id": "5cac49a2976d5bab9c40fb647dc5239b46758e76",
+          "message": "Revert \"test\"\n\nThis reverts commit 59163d1a0bc75cc9749561d37d7e801a15ed5133.",
+          "timestamp": "2022-07-01T17:05:06+08:00",
+          "tree_id": "219f3525a1d779471898d62813c8cbd77d9a258c",
+          "url": "https://github.com/GanZiheng/agatedb/commit/5cac49a2976d5bab9c40fb647dc5239b46758e76"
+        },
+        "date": 1656666707624,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "format make key with ts",
+            "value": 116,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "format get ts",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "table builder",
+            "value": 99341341,
+            "range": "± 235292",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "table read",
+            "value": 2856956,
+            "range": "± 40514",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "table read and build",
+            "value": 7964977,
+            "range": "± 22940",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "table random read",
+            "value": 1594,
+            "range": "± 19",
             "unit": "ns/iter"
           }
         ]
